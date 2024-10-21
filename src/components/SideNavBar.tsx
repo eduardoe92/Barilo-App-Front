4c3c64c02994b9715b7aee9f0b2a566c01e152fe
eduardoe92 <eduardo.elias.1192@gmail.com>
2024-10-21 02:12:35 -0300

    Navbar

--- src/components/SideNavBar.tsx
+++ src/components/SideNavBar.tsx
@@ -84,14 +84,14 @@ export default function SideNavBar() {
       ref={sideNavRef}
       className={`shadow-md h-screen bg-white z-50 flex flex-col items-center justify-between fixed top-0 duration-100 text-primary-pink ${
         open ? "w-60" : "w-14"
-      } md:container md:relative md:flex-row md:h-20 md:w-auto md:px-5 py-8 md:mx-auto md:shadow-none`}
+      } md:container md:relative md:flex-row md:h-20 lg:w-auto lg:px-5 py-8 md:mx-auto md:shadow-none`}
     >
       <div className="flex flex-row items-center justify-between h-20 md:p-0 md:items-start md:h-14">
         <Link
           to="/"
           className={`${
             !open && "hidden"
-          } h-16 w-4/5 md:flex md:items-center md:pl-5`}
+          } h-16 w-4/5 md:flex md:items-center lg:pl-5`}
         >
           <TextIconComponent />
         </Link>
@@ -109,12 +109,12 @@ export default function SideNavBar() {
       <ul
         className={`${!open && "items-center"} ${
           open && " w-11/12 md:w-auto"
-        } flex flex-col h-2/4 sm:flex-row sm:items-center md:h-auto`}
+        } flex flex-col h-2/4 md:flex-row md:items-center md:h-auto`}
       >
         {menuItems.map((item, index) => (
           <li
             key={index}
-            className="relative flex items-center gap-2 p-2 my-2 duration-300 rounded-md cursor-pointer hover:bg-primary-purple group sm:pr-5 md:hover:bg-white md:pr-10"
+            className="relative flex items-center gap-2 p-2 my-2 duration-300 rounded-md cursor-pointer hover:bg-primary-purple group md:pr-1 md:hover:bg-white lg:pr-5"
           >
             <a
               href={item.href}
@@ -164,7 +164,7 @@ export default function SideNavBar() {
             <p
               className={`${
                 !open && "w-0 translate-x-24"
-              }  h-2/3 duration-500 overflow-hidden md:overflow-visible md:translate-x-0 md:hidden `}
+              }  text-lg font-bold tracking-wider duration-300 overflow-hidden md:overflow-visible md:translate-x-0 md:hidden `}
             >
               {t("navbar.items.language")}
             </p>
@@ -182,7 +182,7 @@ export default function SideNavBar() {
         </div>
       </ul>
       {/* footer */}
-      <div className={`${open && "hidden"} sm:hidden`}>
+      <div className={`${open && "hidden"} md:hidden`}>
         <SocialNetworks className={"flex flex-col gap-4 mb-4"} />
       </div>
       <div className={`${!open && "hidden"} md:hidden md:h-0`}>
