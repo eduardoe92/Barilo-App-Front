3e374966f33d353d9895848bf6a6223dae313a58
eduardoe92 <eduardo.elias.1192@gmail.com>
2024-11-01 03:02:28 -0300

    types

---
+++ src/types/Pay.ts
@@ -0,0 +1,11 @@
+export interface PaymentProps {
+  name: string;
+  info: string;
+  img: string;
+}
+
+export interface PaymentHistoryProps {
+  amount: number;
+  type: string;
+  date: string;
+}
